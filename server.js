a1d13a5550dc9fa59f20dc1c1b60a30d4e6266f2
spirit1269 <kimberlydjenkins1@gmail.com>
2023-04-07 15:24:13 -0500

    "Commit 3"

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 //Declare dependencies
-const doetenv = require('dotenv')
+const dotenv = require('dotenv')
 const express = require('express');
 const app = express();
 const {Pool} = require("pg");
 
 //Initialize dotenv
-doetenv.config();
+dotenv.config();
 
 //Initialize Express
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
 //Initialize PG
-const pool = new Pool({DB_URL:process.env.DATABASE_URL})
+const pool = new Pool({connectionString:process.env.DATABASE_URL})
 pool.connect();
 
 app.get('/', (req, res) => {
-    res.json({"Welcome to the Yarn DB"})
+    res.send("Welcome to the Yarn DB")
 })
 app.get('/yarn', (req, res) => {
     pool
@@ -25,13 +25,8 @@ app.get('/yarn', (req, res) => {
             console.log(result)
             res.send(result.rows)
         })
-    .cath((e) => console.error(e.stack))
+    .catch((e) => console.error(e.stack))
 })
-app.post('/yarn', (req, res)=>{
-    pool
-        .query('SELECT * FROM yarn WHERE id = $1' , [req.params.yarnID])
-        .then(res.send(result.rows))
-    })
 
 app.post('/yarn', (req, res) => {
        pool.query('INSERT INTO yarn (name, size, fiber_type, brand) VALUES ($1, $2, $3, $4) RETURNING * ',[
@@ -43,20 +38,17 @@ app.post('/yarn', (req, res) => {
          .then((RESULT) => {
             res.send(result.rows);
         })
-    });    
-app.patch('/yarn/:yarnID', (req, res)=> {
-       let key = OBJECT.keys(req.body)[0]
-        let value = OBJECT.values(req.body)[0]
-        pool.query('UPDATE yarn SET $1 = $2 WHERE id = $3')
-})
+    }); 
 
-// Route handler for creating a yarn
-app.post('/yarn', (req, res) => {
-    const yarns = req.body;
-    yarn.id = yarn.length + 1;
-    yarn.push(yarns);
-    res.status(200).json(yarns);
-  });
+app.patch('/yarn/:yarnID', (req, res) => {
+      let key = Object.keys(req.body)[0];
+      let value = Object.values(req.body)[0];
+      pool.query('UPDATE yarn SET ' + key + ' = $1 WHERE id = $2', [value, req.params.yarnID])
+        .then((result) => {
+            res.send(result.rows);
+        })
+        .catch((e) => console.error(e.stack));
+    });
 
 app.listen(port, (error) => {
     if (error) {
